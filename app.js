aca03a73561644a00c362945cf643f1b8bdb5be9
Pedro Neves <pedro24319245@gmail.com>
2026-05-31 00:04:46 -0300

    Alteração do arquivo app.js.

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
 function sortear() {
-    var numero = Math.floor(Math.random() * 100) + 1;
-    document.getElementById("resultado").innerHTML = "O número sorteado é: " + numero;
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+
+    let sorteados = [];
+    for (let i = 0; i < quantidade; i++) {
+        let numeroSorteado = obeterNumeroAleatorio(de, ate);
+        while (sorteados.includes(numeroSorteado)) {
+            numeroSorteado = obeterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numeroSorteado);
+    }
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarBotaoReiniciar();
+}
+
+function obeterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarBotaoReiniciar() {
+    let botao = document.getElementById("btn-reiniciar");
+    if (botao.classList.contains("container__botao-desabilitado")) {
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao");
+    } else {
+        botao.classList.remove("container__botao");
+        botao.classList.add("container__botao-desabilitado");
+    }
+}
+
+function reiniciar() {
+    document.getElementById("quantidade").value = "";
+    document.getElementById("de").value = "";
+    document.getElementById("ate").value = "";
+    document.getElementById("resultado").innerHTML = "<label class='texto__paragrafo'>Números sorteados: </label>";
+    alterarBotaoReiniciar();
 }
